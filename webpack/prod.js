4f809e7d0aab3499cfc225208305b049492f535b
ahmad rizki maulana <ahmadrizkimaulana666@gmail.com>
2025-08-24 11:30:57 +0700

    update

--- webpack/prod.js
+++ webpack/prod.js
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 const base = require('./base');
 
 module.exports = merge(base, {
@@ -12,6 +13,13 @@ module.exports = merge(base, {
     maxEntrypointSize: 900000,
     maxAssetSize: 900000,
   },
+  plugins: [
+    new Dotenv({
+      path: './.env', // Path to .env file
+      systemvars: true, // Load system environment variables
+      safe: true, // Load .env.example if .env is missing
+    }),
+  ],
   optimization: {
     minimizer: [
       new TerserPlugin({
